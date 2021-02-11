c40282bd8c568faf2d7478553aa1e4e45821bfca
BrunaAQ <49010864+BrunaAQ@users.noreply.github.com>
2021-02-11 15:23:43 -0300

    Inserção de função

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function explorar(){
     alert("💡 Que tal se guiar pelo nosso menu!?\n\n🔎 Nele, estão todas as seções da nossa página \n\n📚💻 Desejamos uma boa leitura 🤜🤛");
 }
+
+function baixar() {
+    window.open("https://drive.google.com/drive/folders/1wNo_5EqgwegAS-gMlpide8uArfKBc-U1");
+    
+}
